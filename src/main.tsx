98837cc35808f54c5a43cff57ca353659e62aa6d
hasanraza38 <92hasanraza689@gmail.com>
2025-02-01 23:32:38 +0500

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -33,9 +33,9 @@ const router = createBrowserRouter([
   },
 ]);
 
-createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router}>
-    <Home/>
-  </RouterProvider>
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
 );
+
 
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,24 @@
 import { Button } from "@/components/ui/button"
 
 const Home = () => {
+
+function click (){
+    console.log("button clicked");
+    
+}
+
+
+
+    
   return (
+
+    <>
     <div className="flex justify-center items-center h-[100vh] gap-5">
         <h1>Home</h1>
-        <Button>Click</Button>
+        <Button variant="outline" onClick={click}>Click</Button>
     </div>
+    </>
+    
   )
 }
 
