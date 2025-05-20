e9cfed736618b323a431907b670a6e6081aad4b8
HASAN RAZA <92hasanraza689@gmail.com>
2025-05-20 23:23:35 +0500

    updates

--- server/index.js
+++ server/index.js
@@ -11,22 +11,32 @@ import dashboardRoutes from "./src/routes/dashboard.routes.js";
 
 const app = express();
 const port = process.env.PORT;
-
+const allowedOrigins = [
+ "https://shopsy1.vercel.app/",
+];
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://shopsy1.vercel.app/",
-    credentials: true,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   })
 );
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:5173"); 
-//   res.header("Access-Control-Allow-Credentials", "true"); 
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
+// app.use(
+//   cors({
+//     origin: "https://shopsy1.vercel.app/",
+//     credentials: true,
+//   })
+// );
+
 
 
 app.get("/", (req, res) => {
