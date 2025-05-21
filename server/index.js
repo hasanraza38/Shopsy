b46984ed4c4982b0051921abe2ad726e31b38a8f
HASAN RAZA <92hasanraza689@gmail.com>
2025-05-22 00:09:43 +0500

    updates

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://shopsy1.vercel.app/",
+    origin: "https://shopsy1.vercel.app",
     credentials: true,
   })
 );
